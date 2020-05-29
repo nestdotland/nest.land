762919722fa8cf2d7c49fbf6bf1ac138a2d3e41d
zorbyte <zorbytee@gmail.com>
2020-05-29 21:36:56 +1000

    src: Add logger and organise a bit more.

---
+++ api/src/deps.ts
@@ -0,0 +1,11 @@
+export {
+  gray,
+  cyan,
+  yellow,
+  red,
+  green,
+  magenta,
+  bold,
+} from "https://deno.land/std@0.53.0/fmt/colors.ts";
+export { Application, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+export { Snelm } from "https://deno.land/x/snelm@1.2.0/mod.ts";
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,9 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { Snelm } from "https://deno.land/x/snelm@1.2.0/mod.ts";
+import { Application, Router, Snelm } from "./deps.ts";
+import { createLogger } from "./utils/logger.ts";
+
+const log = createLogger();
+
+log.debug("Starting nest.land API");
 
 const app = new Application();
 const router = new Router();
@@ -20,5 +24,12 @@ router.get("/", (ctx) => {
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  log.info(
+    `Listening on: ${secure ? "https://" : "http://"}${hostname ??
+      "localhost"}:${port}`,
+  );
+});
 
 await app.listen({ port: 8080 });
