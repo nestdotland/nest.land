9727e52d5827bab1848c008fa898ed9db05025fc
tate <tateflyer1@gmail.com>
2020-06-11 16:39:59 -0400

    fix[CLI]: Fixed CLI to be installable from the registry

--- eggs/mod.ts
+++ eggs/mod.ts
@@ -1,1 +1,9 @@
-// this is a stub.
+import { Command } from "./src/deps.ts";
+import { publish } from "./src/commands/publish.ts";
+
+await new Command()
+  .name("eggs")
+  .version("0.0.1")
+  .description("nest.land - A package registry for Deno, on the permaweb")
+  .command("publish", publish)
+  .parse(Deno.args);
--- eggs/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "./deps.ts";
-import { publish } from "./commands/publish.ts";
-
-await new Command()
-  .name("eggs")
-  .version("0.0.1")
-  .description("nest.land - A package registry for Deno, on the permaweb")
-  .command("publish", publish)
-  .parse(Deno.args);
