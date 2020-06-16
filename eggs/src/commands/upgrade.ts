154a648b3a205537e622421876931f0c8e7a8619
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-16 13:45:21 +0530

    feat: add Deno.run to upgrade

--- eggs/src/commands/upgrade.ts
+++ eggs/src/commands/upgrade.ts
@@ -11,5 +11,10 @@ export const upgrade = new Command()
   .description("Upgrade the current nest.land CLI.")
   .action(async () => {
     // TODO: add cli upgrade command
+    let upagrdeProcess = Deno.run({
+      cmd: ["deno", "install", "--unstable", "-A", "-f", "-n", "eggs", "https://x.nest.land/eggs@0.1.0/mod.ts"],
+      stdout: "piped",
+      stderr: "piped"
+    });
     console.log(bold(green("Successfully upgraded eggs cli!")));
   });
