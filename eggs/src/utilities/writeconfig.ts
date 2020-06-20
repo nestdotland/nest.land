3eb4ac2259ef38df5aa0ab3c1646b7b2228257ac
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-20 20:32:10 +0530

    chores(eggs): some formatting

--- eggs/src/utilities/writeconfig.ts
+++ eggs/src/utilities/writeconfig.ts
@@ -2,7 +2,6 @@ import { writeJson, stringify } from "../deps.ts";
 import { Config, ConfigFormats } from "../types.ts";
 
 export async function writeConfig(data: Config, format: ConfigFormats) {
-
   (format === "yaml" || "yml") ?
   await writeYaml(`egg.${format}`, stringify(data)) :
   await writeJson("egg.json", data, { spaces: 2 });
