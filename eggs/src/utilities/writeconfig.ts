416cd79778754c424d63515e10260a9895747bc5
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-20 20:20:50 +0530

    feat: stringify yaml data

--- eggs/src/utilities/writeconfig.ts
+++ eggs/src/utilities/writeconfig.ts
@@ -1,9 +1,9 @@
-import { writeJson, parse } from "../deps.ts";
+import { writeJson, stringify } from "../deps.ts";
 import { Config, ConfigFormats } from "../types.ts";
 
 export async function writeConfig(data: Config, format: ConfigFormats) {
   (format === "yaml" || "yml") ?
-  await writeYaml(`egg.${format}`, data) :
+  await writeYaml(`egg.${format}`, stringify(data)) :
   await writeJson("egg.json", data, { spaces: 2 });
 }
 
