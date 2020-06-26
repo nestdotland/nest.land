2468814a73b7fbe7fca12ffffc2ef85e937a5ceb
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-26 21:08:12 +0530

    feat(api-next)[mutations]: add new user db handler.

--- api-next/src/db.rs
+++ api-next/src/db.rs
@@ -1,6 +1,6 @@
 // Postgres database management for Nest API
 
-use crate::schema::{Package, User};
+use crate::schema::{Package, User, NewUser};
 use crate::utils::first;
 use chrono::{DateTime, Utc};
 use postgres_array::array::Array;
@@ -70,6 +70,28 @@ pub async fn get_user_by_key(db: Arc<Client>, apiKey: String) -> Result<User, St
             apiKey: row.get(3),
             packageNames: packageNames.iter().cloned().collect(),
             createdAt: format!("{:?}", row.get::<usize, DateTime<Utc>>(5)),
+        })
+    } else {
+        Err("Not found".to_string())
+    }
+}
+
+// Method to create a user
+pub async fn create_user(db: Arc<Client>, newUser: NewUser) -> Result<User, String> {
+    let rows = &db
+        .query("INSERT INTO users (name, normalizedName, password, apiKey, packageNames, createdAt) VALUES ($1, $2, $3, $4, $5, $6)", &[&newUser.name, &newUser.name, &newUser.password, &"apiKey", &Array::<Vec<String>>::from_vec(vec![], 0), &Utc::now()])
+        .await
+        .unwrap();
+    let _row = first(rows);
+    if let Some(x) = _row {
+        let row = _row.unwrap();
+        let packageNames: Array<String> = row.get(4);
+        Ok(User {
+            name: newUser.name,
+            normalizedName: "newUser.name".to_owned(),
+            apiKey: "apiKey".to_owned(),
+            packageNames: vec![],
+            createdAt: format!("{:?}", Utc::now()),
         })
     } else {
         Err("Not found".to_string())
