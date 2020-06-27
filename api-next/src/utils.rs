e5a1a213e37bcf02088b3727d96a1a49e6916e2b
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-27 16:08:07 +0530

    chores: add comments to utils

--- api-next/src/utils.rs
+++ api-next/src/utils.rs
@@ -6,10 +6,12 @@ pub fn first<T>(v: &Vec<T>) -> Option<&T> {
     v.first()
 }
 
+// generate a random api_key
 pub fn create_api_key() -> String {
     Uuid::new_v4().to_simple().to_string()
 }
 
+// normalize string
 pub fn normalize(input: &str) -> String {
     input.to_lowercase().chars().map(|x| match x {
         '!' => '_',
