0c6c40260bf42d452e8a17582a68ebd7d40a7499
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-27 14:46:52 +0530

    feat: new util create_api_key

--- api-next/src/utils.rs
+++ api-next/src/utils.rs
@@ -1,6 +1,11 @@
 // utils used by the nestapi.
+use uuid::Uuid;
 
 // get the first element of a vector
 pub fn first<T>(v: &Vec<T>) -> Option<&T> {
     v.first()
+}
+
+pub fn create_api_key() -> String {
+    Uuid::new_v4().to_simple().to_string()
 }
