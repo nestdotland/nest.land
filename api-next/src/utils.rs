4edcbecd320483aa61d28fac96e81ec8ef4b2010
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-27 15:20:52 +0530

    feat: normalize username

--- api-next/src/utils.rs
+++ api-next/src/utils.rs
@@ -9,3 +9,14 @@ pub fn first<T>(v: &Vec<T>) -> Option<&T> {
 pub fn create_api_key() -> String {
     Uuid::new_v4().to_simple().to_string()
 }
+
+pub fn normalize(input: &str) -> String {
+    input.to_lowercase().chars().map(|x| match x {
+        '!' => '_',
+        '[' => '_',
+        ']' => '_',
+        '.' => '_',
+        ':' => '_',
+        _ => x
+    }).collect()
+}
