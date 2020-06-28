0bc341902b65848442a8661a9693d4484de2003e
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-28 20:47:19 +0530

    chores(twig): some formatting

--- twig/src/recursive_read.ts
+++ twig/src/recursive_read.ts
@@ -1,28 +1,34 @@
 import glob from "glob";
 import * as path from "path";
 
-export default function globPromise (dir, asObject = false) {
-    return new Promise ((resolve, reject) => {
-        glob(path.resolve(`${dir}/**/*`), {strict: false, silent: true, nodir: true}, (err, files) => {
-            if (err) {
-                reject(err);
-            } else {
-                if (asObject) {
-                    let filesObject = files.map(file => {
-                        let regexp = /^(.*[\\\/])(.*)$/;
-                        let match = regexp.exec(file);
-                        return {
-                            fullpath: file,
-                            filepath: match[1],
-                            filename: match[2],
-                            dirname: regexp.exec(match[1].substring(0, match[1].length -1))[2],
-                        };
-                    })
-                    resolve(filesObject);
-                } else {
-                    resolve(files);
-                }
-            }
-        })
-    })
+export default function globPromise(dir, asObject = false) {
+  return new Promise((resolve, reject) => {
+    glob(
+      path.resolve(`${dir}/**/*`),
+      { strict: false, silent: true, nodir: true },
+      (err, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (asObject) {
+            let filesObject = files.map(file => {
+              let regexp = /^(.*[\\\/])(.*)$/;
+              let match = regexp.exec(file);
+              return {
+                fullpath: file,
+                filepath: match[1],
+                filename: match[2],
+                dirname: regexp.exec(
+                  match[1].substring(0, match[1].length - 1),
+                )[2],
+              };
+            });
+            resolve(filesObject);
+          } else {
+            resolve(files);
+          }
+        }
+      },
+    );
+  });
 }
