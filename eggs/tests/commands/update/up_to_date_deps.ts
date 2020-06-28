cb06f5fd943a17ecca36ef8695cb221a805b3dfa
tate <tateflyer1@gmail.com>
2020-06-28 16:17:04 -0400

    fix: Removed 'v' in front of eggs module versions

--- eggs/tests/commands/update/up_to_date_deps.ts
+++ eggs/tests/commands/update/up_to_date_deps.ts
@@ -1,4 +1,4 @@
 import * as colors from "https://deno.land/std@v0.59.0/fmt/colors.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.1/mod.ts"
-import * as eggs from "https://x.nest.land/eggs@v0.1.6/mod.ts"
+import * as eggs from "https://x.nest.land/eggs@0.1.6/mod.ts"
 import * as http from "https://deno.land/std/http/mod.ts" // not versioned for a reason, to ensure we test this case
