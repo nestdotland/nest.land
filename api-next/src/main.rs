dcac9387871971ef2f5060c73f5734998ec8e20f
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-29 16:21:38 +0530

    rust-api: store file in temp

--- api-next/src/main.rs
+++ api-next/src/main.rs
@@ -53,7 +53,7 @@ async fn upload_package(mut payload: Multipart) -> Result<HttpResponse, Error> {
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
         let filename = content_type.get_filename().unwrap();
-        let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
+        let filepath = format!("../twig/tmp/{}", sanitize_filename::sanitize(&filename));
         // File::create is blocking operation, use threadpool
         let mut f = web::block(|| std::fs::File::create(filepath))
             .await
