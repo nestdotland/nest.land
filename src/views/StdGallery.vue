b696b77a10ffb796b0ee50c2a2e46dc59abf08fe
Marton <martondeveloper@gmail.com>
2020-07-18 00:43:31 +0200

    Finished std hero

--- src/views/StdGallery.vue
+++ src/views/StdGallery.vue
@@ -10,8 +10,17 @@
             <div class="std-hero">
               <denologo />
               <div class="std-description">
-                <h1 class="title is-1">The Std Gallery <span class="std-version">{{ version }}</span></h1>
+                <h1 class="title is-1">The std gallery <span class="std-version">{{ version }}</span></h1>
                 <p class="subtitle">This is a mirror of Deno's Standard Modules. The code itself is not maintained, modified, or updated by nest.land. New versions are published when they are released by Deno. <br>These modules do not have external dependencies and they are reviewed by the Deno core team. The intention is to have a standard set of high quality code that all Deno projects can use fearlessly.</p>
+              </div>
+            </div>
+            <div class="std-info">
+              <p>Published by the <span>nest.land team</span> on 05/10/2020</p>
+              <div class="select is-light has-light-arrow is-fullwidth">
+                <select v-model="version">
+                  <option value="1.0.0">1.0.0</option>
+                  <option value="0.0.9">0.0.9</option>
+                </select>
               </div>
             </div>
           </div>
@@ -94,10 +103,38 @@
     display: flex
     align-items: center
     justify-content: space-between
+    margin-bottom: 2em
+
+    @media screen and (max-width: $mobileScreenMaxWidth)
+      display: block
 
     svg
       width: 17%
 
+      @media screen and (max-width: $mobileScreenMaxWidth)
+        display: none
+
     .std-description
       width: 78%
+
+      @media screen and (max-width: $mobileScreenMaxWidth)
+        width: 100%
+
+        span.std-version
+          display: block
+
+  .std-info 
+    display: flex
+    align-items: center
+    justify-content: space-between
+
+    p
+      color: $accentColor
+
+      span
+        border-bottom: 1px solid $accentColor
+
+    .select
+      width: 120px
+
 </style>
