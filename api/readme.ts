cd42f3035169882101df2bfe6c845d1e1bcdf291
Marton Lederer <marton@lederer.hu>
2020-10-04 23:36:41 +0200

    api: readme.ts

---
+++ api/readme.ts
@@ -0,0 +1,50 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import axios from "axios";
+
+export default (req: NowRequest, res: NowResponse) => {
+  const pkg = req.body.data["pkg"];
+  if(pkg === null || pkg === undefined || pkg === "") res.status(500).json({ message: "Wrong request" });
+
+  axios
+    .get(`https://x.nest.land/${pkg}/README.md`)
+    .then(({ data }) => {
+      // replace html href attrs
+      data = data.replace(
+        /(?<=((href)=("|')))(?!(http|https))([^\s\\]*)(?=("|'))/g,
+        (replaceVal) =>
+          `/package/${pkg.split("@")[0]}/files/${replaceVal.replace(
+            /\/|\.\//,
+            ""
+          )}`
+      );
+      // replace html src attrs
+      data = data.replace(
+        /(?<=((src)=("|')))(?!(http|https))([^\s\\]*)(?=("|'))/g,
+        (replaceVal) =>
+          `https://x.nest.land/${pkg}/${replaceVal.replace(
+            /\/|\.\//,
+            ""
+          )}`
+      );
+      // replace imgs
+      data = data.replace(
+        /(?<=((\!\[(.*)\])\())(?!(http|https))([^\s\\]*)(?=(\)))/g,
+        (replaceVal) =>
+          `https://x.nest.land/${pkg}/${replaceVal.replace(
+            /\/|\.\//,
+            ""
+          )}`
+      );
+      // replace links
+      data = data.replace(
+        /(?<!((!\[(.*)\])\())(?<=((\[(.*)\])\())(?!(http|https))([^\s\\]*)(?=(\)))/g,
+        (replaceVal) =>
+          `/package/${pkg.split("@")[0]}/files/${replaceVal.replace(
+            /\/|\.\//,
+            ""
+          )}`
+      );
+      res.status(200).send(data);
+    })
+    .catch(({ response }) => res.status(response.status).json({ message: "There was an error while fetching the README." }));
+}
